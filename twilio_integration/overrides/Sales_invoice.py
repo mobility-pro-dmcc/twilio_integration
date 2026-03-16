179add671eb28c294adf53476cffb31d83a80d14
Ahmed Zaytoon <citybirdman@gmail.com>
2026-03-16 14:11:04 +0000

    use get_controller instead of class

--- twilio_integration/overrides/Sales_invoice.py
+++ twilio_integration/overrides/Sales_invoice.py
@@ -1,8 +1,11 @@
 import frappe
-from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
+from importlib import import_module
+from frappe.model.base_document import get_controller
+
 
 def check_custom_condition(self):
     return frappe.db.get_value("Customer", self.customer, "send_whatsapp_notifications")
     
 def monkey_patch_notifications():
+    SalesInvoice = get_controller("Sales Invoice")
     SalesInvoice.send_notification = property(check_custom_condition)
