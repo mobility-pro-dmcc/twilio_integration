4511b1fd6f2139c77a4aa01a7cabef217aa31c27
Ahmed Zaytoon <citybirdman@gmail.com>
2026-04-09 14:45:56 +0000

    Fix: send notification error

--- twilio_integration/__init__.py
+++ twilio_integration/__init__.py
@@ -2,7 +2,5 @@
 from __future__ import unicode_literals
 
 __version__ = '0.0.1'
-
-from twilio_integration.overrides.sales_invoice import monkey_patch_notifications
+from .overrides.sales_invoice import monkey_patch_notifications
 monkey_patch_notifications()
-
--- twilio_integration/overrides/sales_invoice.py
+++ twilio_integration/overrides/sales_invoice.py
@@ -1,8 +1,12 @@
 import frappe
-from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
+from importlib import import_module
+from frappe.model.base_document import get_controller
+
 
 def check_custom_condition(self):
     return frappe.db.get_value("Customer", self.customer, "send_whatsapp_notifications")
     
 def monkey_patch_notifications():
+    from override_outstanding_function_in_erpnext.overrides.sales_invoice import CustomSalesInvoice as SalesInvoice
+    #SalesInvoice = get_controller("Sales Invoice")
     SalesInvoice.send_notification = property(check_custom_condition)
